c168bfc68c045977f8685dca74496b1a6c166049
yigitdelikanli <102251349+yigitdelikanli@users.noreply.github.com>
2022-08-03 21:44:11 +0300

    Add files via upload

---
+++ akkaDemo/src/main/java/example/GreeterMain.java
@@ -0,0 +1,44 @@
+package example;
+
+import akka.actor.typed.ActorRef;
+import akka.actor.typed.Behavior;
+import akka.actor.typed.javadsl.*;
+
+public class GreeterMain extends AbstractBehavior<GreeterMain.SayHello> {
+
+    public static class SayHello {
+        public final String name;
+
+        public SayHello(String name) {
+            this.name = name;
+        }
+    }
+
+    private final ActorRef<Greeter.Greet> greeter;
+
+    public static Behavior<SayHello> create() {
+        return Behaviors.setup(GreeterMain::new);
+    }
+
+    private GreeterMain(ActorContext<SayHello> context) {
+        super(context);
+        //#create-actors
+        greeter = context.spawn(Greeter.create(), "greeter");
+        //#create-actors
+    }
+
+    @Override
+    public Receive<SayHello> createReceive() {
+        return newReceiveBuilder().onMessage(SayHello.class, this::onSayHello).build();
+    }
+
+    private Behavior<SayHello> onSayHello(SayHello command) {
+        //#create-actors
+        //only greet once
+        ActorRef<Greeter.Greeted> replyTo =
+                getContext().spawn(GreeterBot.create(1), command.name);
+        greeter.tell(new Greeter.Greet(command.name, replyTo));
+        //#create-actors
+        return this;
+    }
+}
